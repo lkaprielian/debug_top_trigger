e24487d5e91bf32817c1e883275c1717254dabb3
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-17 15:30:25 +0100

    Update reports.availreport.view.html.php

--- partials/reports.availreport.view.html.php
+++ partials/reports.availreport.view.html.php
@@ -14,7 +14,7 @@ $table->setHeader([
 	(new CColHeader(_('Tags')))
 ]);
 
-print_r($view_url);
+print_r($data);
 $allowed_ui_problems = CWebUser::checkAccess(CRoleHelper::UI_MONITORING_PROBLEMS);
 $triggers = $data['triggers'];
 
