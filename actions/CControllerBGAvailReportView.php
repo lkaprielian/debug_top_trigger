456596682a76cf901c0f2cf489c61da0b0ceabdc
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-17 18:55:30 +0100

    Update CControllerBGAvailReportView.php

--- actions/CControllerBGAvailReportView.php
+++ actions/CControllerBGAvailReportView.php
@@ -61,10 +61,9 @@ class CControllerBGAvailReportView extends CControllerBGAvailReport {
 			if ($index == $profile->selected) {
 				// Initialize multiselect data for filter_scr to allow tabfilter correctly handle unsaved state.
 				$filter_tab['filter_src']['filter_view_data'] = $this->getAdditionalData($filter_tab['filter_src']);
-				
 			}
 
-			$filter_tabs[] = [[]];
+			$filter_tabs[] = $filter_tab + ['filter_view_data' => $this->getAdditionalData($filter_tab)];
 		}
 
 		// filter
@@ -92,7 +91,7 @@ class CControllerBGAvailReportView extends CControllerBGAvailReport {
 					'disabled' => false
 				] + getTimeselectorActions($profile->from, $profile->to)
 			],
-			'filter_tabs' => $filter_tabs,
+			'filter_tabs' => [[]],
 			'refresh_url' => $refresh_curl->getUrl(),
 			'refresh_interval' => CWebUser::getRefresh() * 10000, //+++1000,
 			'page' => $this->getInput('page', 1)
