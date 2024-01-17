3de213b2de238193078bc66e74f12a89c485a8eb
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-18 00:53:13 +0100

    Update reports.availreport.filter.php

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -131,6 +131,7 @@ $template = (new CForm('get'))
 	->addItem([
 		$template,
 		(new CSubmitButton(null))->addClass(ZBX_STYLE_DISPLAY_NONE),
+		(new CVar('action', 'availreport.view.refresh'))->removeId(),
 		(new CVar('filter_name', '#{filter_name}'))->removeId(),
 		(new CVar('filter_show_counter', '#{filter_show_counter}'))->removeId(),
 		(new CVar('filter_custom_time', '#{filter_custom_time}'))->removeId(),
