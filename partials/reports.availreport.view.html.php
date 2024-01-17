5dd836c67ee6c3d405ec765ac9b89dc5341f3e26
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-18 00:15:27 +0100

    Update reports.availreport.view.html.php

--- partials/reports.availreport.view.html.php
+++ partials/reports.availreport.view.html.php
@@ -5,7 +5,8 @@ $form = (new CForm())->setName('availreport_view');
 $table = (new CTableInfo());
 
 $view_url = $data['view_curl']->getUrl();
-print_r($data['filter']['from']);
+// print_r($data['filter']['from']);
+print_r($data);
 // $arr = explode('&', $view_url, -1);
 // // $key = array_search('from=', $arr);
 
