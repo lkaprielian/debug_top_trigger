b9ddb81aec0b4e539b7cc59d2e83a20c031b3f3d
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-17 13:27:06 +0100

    Update reports.availreport.filter.php

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -122,7 +122,7 @@ $template = (new CForm('get'))
 		(new CVar('filter_name', '#{filter_name}'))->removeId(),
 		(new CVar('filter_show_counter', '#{filter_show_counter}'))->removeId(),
 		(new CVar('filter_custom_time', '#{filter_custom_time}'))->removeId(),
-		(new CVar('from', 'now-'.CSettingsHelper::get(CSettingsHelper::PERIOD_DEFAULT))),
+		(new CVar('from', 'now')),
 		(new CVar('to', '#{to}'))->removeId()
 	]);
 
