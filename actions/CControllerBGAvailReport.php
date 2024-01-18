255923092f5c51c8c87ce1f902af86251fe8732d
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-18 01:32:54 +0100

    Update CControllerBGAvailReport.php

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -304,16 +304,12 @@ abstract class CControllerBGAvailReport extends CController {
 			$data['hosts_multiselect'] = CArrayHelper::renameObjectsKeys(array_values($hosts), ['hostid' => 'id']);
 		}
 		
-		if ($filter['from'] != '' && $filter['to'] != '') {
+		if ($filter['from'] == '') {
 			$range_time_parser = new CRangeTimeParser();
 			$range_time_parser->parse($filter['from']);
-			$filter['from_ts'] = $range_time_parser->getDateTime(true)->getTimestamp(); // timestamp for sql request
-			$range_time_parser->parse($filter['to']);
-			$filter['to_ts'] = $range_time_parser->getDateTime(false)->getTimestamp(); // timestamp for sql request
-		} else {
-			$filter['from_ts'] = null; //nothing changed
-			$filter['to_ts'] = null;
-		}
+			$filter['from'] = $range_time_parser->getDateTime(true)->getTimestamp(); // timestamp for sql request
+			$data['from'] = $filter['from'];
+		} 
 		// if ($filter['from']) {
 		// 	$data['from'] = 'now-7d';
 		// }
